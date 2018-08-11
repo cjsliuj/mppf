1a958758a2098231b71f524b4b214340392cbb49
cjsliuj@163.com <cjsliuj@163.com>
2018-08-12 01:51:42 +0800

    update setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python
 # -*- coding:utf-8 -*-
-from setuptools import setup, find_packages
+from setuptools import setup
 setup(
     name="mppf",
-    version="1",
+    version="1.0",
     author="nsrx",
     author_email="cjsliuj@163.com",
     description="A simple command line tool for managing provisioning profiles.",
-    url="",
+    url="https://github.com/cjsliuj/mppf",
     platforms = "any",
     packages = [
         'source'
